9e0617c589d88a3623c83e5737ccba4dd0278031
martin.lin <martin.lin@ecpay.com.tw>
2022-09-07 10:57:08 +0800

    新增首頁

--- app/src/main/java/com/mrt/mytravellifemap/MainActivity.kt
+++ app/src/main/java/com/mrt/mytravellifemap/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.mrt.mytravellifemap
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        fun startActivity(
+            activity: Activity
+        ) {
+            val intent = Intent(activity, MainActivity::class.java)
+
+            activity.startActivity(intent);
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
