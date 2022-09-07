738eb896dcc3ec21b8a431343818fad35b919958
martin.lin <martin.lin@ecpay.com.tw>
2022-09-07 11:42:04 +0800

    更換移除v7

--- app/src/main/java/com/mrt/mytravellifemap/MRT.kt
+++ app/src/main/java/com/mrt/mytravellifemap/MRT.kt
@@ -1,6 +1,6 @@
 package com.mrt.mytravellifemap
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MRT : AppCompatActivity() {
--- app/src/main/java/com/mrt/mytravellifemap/MainActivity.kt
+++ app/src/main/java/com/mrt/mytravellifemap/MainActivity.kt
@@ -2,7 +2,7 @@ package com.mrt.mytravellifemap
 
 import android.app.Activity
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
