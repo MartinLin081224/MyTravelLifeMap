d0196bd45c3e751b3299150652179005e5ba07e8
martin.lin <martin.lin@ecpay.com.tw>
2022-09-07 10:56:58 +0800

    新增 過場頁面跳轉到首頁

--- app/src/main/java/com/mrt/mytravellifemap/Splash.kt
+++ app/src/main/java/com/mrt/mytravellifemap/Splash.kt
@@ -1,11 +1,45 @@
 package com.mrt.mytravellifemap
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.Animation
 
-class Splash : AppCompatActivity() {
+class Splash : AppCompatActivity(), Animation.AnimationListener {
+    companion object {
+        fun startActivity(
+            activity: Activity,
+        ) {
+            val intent = Intent(activity, Splash::class.java)
+
+            activity.startActivity(intent)
+        }
+    }
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            MainActivity.startActivity(this)
+        }, 2000)
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+        TODO("Not yet implemented")
+        MainActivity.startActivity(this)
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+        TODO("Not yet implemented")
     }
 }
